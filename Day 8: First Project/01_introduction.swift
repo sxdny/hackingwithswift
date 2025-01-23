a9f35ba7efb6a37dc8dd0e5218b165d4dcbef594
sxdny <67187745+sxdny@users.noreply.github.com>
2025-01-23 17:59:03 +0100

    first project done

---
+++ Day 8: First Project/01_introduction.swift
@@ -0,0 +1,20 @@
+// 01 - First Project: WeSplit
+
+/*
+
+The WeSplitApp.swift file contains the code
+for launching the app.
+
+The ContentView.swift contains the initial
+UI for your program, and is where we'll
+be doing all the work in this project.
+
+Assets.xcassets is an asset catalog - collection
+of pictures. You can also add colors, app icons,
+iMessage stickers, etc.
+
+Preview Content is a folder with Preview Assets.scassets
+inside - another assets catalog. This is for
+example images you want to use when you're
+designing your UI.
+*/
